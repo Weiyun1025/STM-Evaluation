290a556575bf0f58d7bfb613560f95afd0f1df5c
1612893408@qq.com <1612893408@qq.com>
2022-09-13 21:10:18 +0800

    rm random port

--- utils.py
+++ utils.py
@@ -320,8 +320,7 @@ def init_distributed_mode(args):
         args.world_size = world_size
 
         if "MASTER_PORT" not in os.environ:
-            port = 22110 + (int(time.time()) % 1000)
-            os.environ["MASTER_PORT"] = str(port)
+            os.environ["MASTER_PORT"] = "22110"
         node_list = os.environ["SLURM_NODELIST"]
         addr = subprocess.getoutput(f"scontrol show hostname {node_list} | head -n1")
         if "MASTER_ADDR" not in os.environ:
