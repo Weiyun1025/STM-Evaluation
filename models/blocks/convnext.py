c2aab1a922e19203dbea35a6dbd4a1639ff42065
1612893408@qq.com <1612893408@qq.com>
2022-09-17 23:21:05 +0800

    add extra ffn for convnext

--- models/blocks/convnext.py
+++ models/blocks/convnext.py
@@ -6,7 +6,7 @@ from ..meta_arch import MetaArch
 
 
 class ConvNeXtBlock(nn.Module):
-    def __init__(self, dim, drop_path, layer_scale_init_value, **kwargs):
+    def __init__(self, dim, drop_path, layer_scale_init_value, extra_ffn=False, **kwargs):
         super().__init__()
         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
         self.norm = nn.LayerNorm(dim, eps=1e-6)
@@ -14,12 +14,23 @@ class ConvNeXtBlock(nn.Module):
         self.pwconv1 = nn.Linear(dim, 4 * dim)
         self.act = nn.GELU()
         self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
-                                  requires_grad=True) if layer_scale_init_value > 0 else None
+
+        self.gamma_1 = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+
+        self.ffn = nn.Sequential(
+            nn.Linear(dim, 4 * dim),
+            nn.GELU(),
+            nn.Linear(4 * dim, dim),
+        ) if extra_ffn else nn.Identity()
+
+        self.gamma_2 = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                    requires_grad=True) if extra_ffn and layer_scale_init_value > 0 else None
+
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
-        x_input = x
+        shortcut = x
         x = self.dwconv(x)
         # (N, C, H, W) -> (N, H, W, C)
         x = x.permute(0, 2, 3, 1)
@@ -27,12 +38,19 @@ class ConvNeXtBlock(nn.Module):
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
-        if self.gamma is not None:
-            x = self.gamma * x
+        if self.gamma_1 is not None:
+            x = self.gamma_1 * x
+        x = shortcut + self.drop_path(x)
+
+        shortcut = x
+        x = self.ffn(x)
+        if self.gamma_2 is not None:
+            x = self.gamma_2 * x
+        x = shortcut + self.drop_path(x)
+
         # (N, H, W, C) -> (N, C, H, W)
         x = x.permute(0, 3, 1, 2)
 
-        x = x_input + self.drop_path(x)
         return x
 
 
@@ -91,11 +109,44 @@ def convnext_tiny(pretrained=False, **kwargs):
 
 
 @ register_model
+def convnext_v2_tiny(pretrained=False, **kwargs):
+    model = MetaArch(img_size=224,
+                     depths=[3, 3, 9, 3],
+                     dims=[96, 192, 384, 768],
+                     stem_type=ConvNeXtStem,
+                     block_type=ConvNeXtBlock,
+                     block_kwargs=dict(extra_ffn=True),
+                     downsample_type=ConvNeXtDownsampleLayer,
+                     head_type=ConvNeXtHead,
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+
+@ register_model
 def unified_convnext_tiny(pretrained=False, **kwargs):
     model = MetaArch(img_size=224,
                      depths=[3, 3, 9, 3],
                      dims=[96, 192, 384, 768],
                      block_type=ConvNeXtBlock,
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+
+@ register_model
+def unified_convnext_v2_tiny(pretrained=False, **kwargs):
+    model = MetaArch(img_size=224,
+                     depths=[3, 3, 9, 3],
+                     dims=[96, 192, 384, 768],
+                     block_type=ConvNeXtBlock,
+                     block_kwargs=dict(extra_ffn=True),
                      **kwargs)
 
     if pretrained:
