585589c28760a8b4e92b56c7ed069cd65e0c77d2
1612893408@qq.com <1612893408@qq.com>
2022-09-21 13:31:27 +0800

    update extra LN

--- models/blocks/convnext.py
+++ models/blocks/convnext.py
@@ -42,13 +42,14 @@ class ConvNeXtBlock(nn.Module):
 class ConvNeXtV2Block(nn.Module):
     def __init__(self, dim, drop_path, layer_scale_init_value, **kwargs):
         super().__init__()
+        self.dw_norm = nn.LayerNorm(dim, eps=1e-6)
         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
-        self.norm = nn.LayerNorm(dim, eps=1e-6)
 
         self.gamma_1 = nn.Parameter(layer_scale_init_value * torch.ones((1, dim, 1, 1)),
                                     requires_grad=True) if layer_scale_init_value > 0 else None
 
         # pointwise/1x1 convs, implemented with linear layers
+        self.pw_norm = nn.LayerNorm(dim, eps=1e-6)
         self.pwconv1 = nn.Linear(dim, 4 * dim)
         self.act = nn.GELU()
         self.pwconv2 = nn.Linear(4 * dim, dim)
@@ -60,7 +61,7 @@ class ConvNeXtV2Block(nn.Module):
 
     def forward(self, x):
         shortcut = x
-        x = self.dwconv(x)
+        x = self.dwconv(self.dw_norm(x))
         if self.gamma_1 is not None:
             x = self.gamma_1 * x
         x = shortcut + self.drop_path(x)
@@ -69,7 +70,7 @@ class ConvNeXtV2Block(nn.Module):
         x = x.permute(0, 2, 3, 1)
 
         shortcut = x
-        x = self.norm(x)
+        x = self.pw_norm(x)
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
