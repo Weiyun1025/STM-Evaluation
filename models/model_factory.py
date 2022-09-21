f54d23bbc578a6a07c714190d96138321e73865e
flyinglynx <861799874@qq.com>
2022-09-21 17:36:04 +0800

    add new models

--- models/model_factory.py
+++ models/model_factory.py
@@ -150,6 +150,26 @@ def pm_swin_tiny(pretrained=False, **kwargs):
 
     return model
 
+#******************* Swin with Conv Stem and Conv transition layer ******************
+@register_model
+def conv_swin_micro(pretrained=False, **kwargs):
+    dims = [ 64 * 2 ** i for i in range(4)]
+    depths = [2, 2, 6, 2]
+    num_heads = [2, 4, 8, 16]
+    window_size = 7
+
+    model = MetaArch(img_size=224,
+                     depths=depths,
+                     dims=dims,
+                     block_type=SwinBlock,
+                     block_kwargs=dict(num_heads=num_heads, window_size=window_size),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
 
 @register_model
 def conv_swin_tiny(pretrained=False, **kwargs):
@@ -172,6 +192,46 @@ def conv_swin_tiny(pretrained=False, **kwargs):
 
 
 @register_model
+def conv_swin_small(pretrained=False, **kwargs):
+    dims = [96 * 2 ** i for i in range(4)]
+    depths = [2, 2, 18, 2]
+    num_heads = [3, 6, 12, 24]
+    window_size = 7
+
+    model = MetaArch(img_size=224,
+                     depths=depths,
+                     dims=dims,
+                     block_type=SwinBlock,
+                     block_kwargs=dict(num_heads=num_heads, window_size=window_size),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+
+@register_model
+def conv_swin_base(pretrained=False, **kwargs):
+    dims = [128 * 2 ** i for i in range(4)]
+    depths = [2, 2, 18, 2]
+    num_heads = [3, 6, 12, 24]
+    window_size = 7
+
+    model = MetaArch(img_size=224,
+                     depths=depths,
+                     dims=dims,
+                     block_type=SwinBlock,
+                     block_kwargs=dict(num_heads=num_heads, window_size=window_size),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+#******************************************************************************
+
+@register_model
 def dcn_v3(pretrained=False, **kwargs):
     dims = [192 * 2 ** i for i in range(4)]
     depths = [4, 4, 18, 4]
@@ -190,7 +250,8 @@ def dcn_v3(pretrained=False, **kwargs):
 
     return model
 
-
+# ******************************************************************
+# PVT v2 with conv stem and conv transition
 @register_model
 def conv_pvt_v2_b0(pretrained=False, **kwargs):
     model = MetaArch(img_size=224,
@@ -207,6 +268,81 @@ def conv_pvt_v2_b0(pretrained=False, **kwargs):
         raise NotImplementedError()
 
     return model
+
+# b1 config
+@register_model
+def conv_pvt_v2_micro(pretrained=False, **kwargs):
+    model = MetaArch(img_size=224,
+                     depths=[2, 2, 2, 2],
+                     dims=[64, 128, 320, 512],
+                     block_type=PvtV2Block,
+                     block_kwargs=dict(num_heads=[1, 2, 5, 8],
+                                       mlp_ratios=[8, 8, 4, 4],
+                                       qkv_bias=True,
+                                       sr_ratios=[8, 4, 2, 1],),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+# B2 config
+@register_model
+def conv_pvt_v2_tiny(pretrained=False, **kwargs):
+    model = MetaArch(img_size=224,
+                     depths=[3, 3, 6, 3],
+                     dims=[64, 128, 320, 512],
+                     block_type=PvtV2Block,
+                     block_kwargs=dict(num_heads=[1, 2, 5, 8],
+                                       mlp_ratios=[8, 8, 4, 4],
+                                       qkv_bias=True,
+                                       sr_ratios=[8, 4, 2, 1],),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+# b3 config
+@register_model
+def conv_pvt_v2_small(pretrained=False, **kwargs):
+    model = MetaArch(img_size=224,
+                     depths=[3, 3, 18, 3],
+                     dims=[64, 128, 320, 512],
+                     block_type=PvtV2Block,
+                     block_kwargs=dict(num_heads=[1, 2, 5, 8],
+                                       mlp_ratios=[8, 8, 4, 4],
+                                       qkv_bias=True,
+                                       sr_ratios=[8, 4, 2, 1],),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+# b5 config
+@register_model
+def conv_pvt_v2_base(pretrained=False, **kwargs):
+    model = MetaArch(img_size=224,
+                     depths=[3, 6, 40, 3],
+                     dims=[64, 128, 320, 512],
+                     block_type=PvtV2Block,
+                     block_kwargs=dict(num_heads=[1, 2, 5, 8],
+                                       mlp_ratios=[8, 8, 4, 4],
+                                       qkv_bias=True,
+                                       sr_ratios=[8, 4, 2, 1],),
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+
+# ******************************************************************
 
 
 @register_model
