fix MACs computation

tools/calulate_mac_param.py
@@ -2,11 +2,10 @@ import argparse
 
 import numpy as np
 import torch
-from ptflops import get_model_complexity_info
+from thop import profile
 import timm
 from timm.models import create_model
 import models
-import models.swin
 
 
 def parse_args():
@@ -15,12 +14,22 @@ def parse_args():
     return parser.parse_args()
 
 
+def sum_submodule_ops(module):
+    ops = 0
+    for m in module.children():
+        if not getattr(m, '__thop_visit', False):
+            ops += sum_submodule_ops(m)
+        ops += m.total_ops
+    module.__thop_visit = True
+    return ops
+
+
 def timm_halo_attn_posemb(module, input, output):
-    module.__flops__ += input[0].numel(
+    module.total_ops += input[0].numel(
     ) / module.dim_head * module.width_rel.numel()
-    module.__flops__ += input[0].numel(
+    module.total_ops += input[0].numel(
     ) / module.dim_head * module.height_rel.numel()
-    module.__flops__ += output.numel()
+    module.total_ops += output.numel()
 
 
 def timm_halo_attn_haloatnn(module, input, output):
@@ -37,17 +46,15 @@ def timm_halo_attn_haloatnn(module, input, output):
     # k shape: B * num_heads, num_blocks, win_size ** 2, dim_head_qk
     # v shape: B * num_heads, num_blocks, win_size ** 2, dim_head_v
     # attn shape: B * num_heads, num_blocks, block_size ** 2, win_size ** 2
-    module.__flops__ += B * num_heads * num_blocks * (block_size**2) * (
+    module.total_ops += B * num_heads * num_blocks * (block_size**2) * (
         dim_head_qk * win_size**2)  # q @ k.transpose(-1, -2)
-    module.__flops__ += B * num_heads * num_blocks * (block_size**2) * (
+    module.total_ops += B * num_heads * num_blocks * (block_size**2) * (
         win_size**2)  # scale
-    module.__flops__ += B * num_heads * num_blocks * (block_size**2) * (
+    module.total_ops += B * num_heads * num_blocks * (block_size**2) * (
         2 * win_size**2)  # softmax
-    module.__flops__ += B * num_heads * num_blocks * (block_size**2) * (
+    module.total_ops += B * num_heads * num_blocks * (block_size**2) * (
         win_size**2) * dim_head_v  # attn @ v
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+    module.total_ops += sum_submodule_ops(module)
 
 
 def layernorm(module, input, output):
@@ -55,18 +62,16 @@ def layernorm(module, input, output):
     batch_flops = np.prod(input.shape)
     if module.elementwise_affine:
         batch_flops *= 2
-    module.__flops__ += int(batch_flops)
+    module.total_ops += int(batch_flops)
 
 
 def swinblock(module, input, output):
     if module.gamma_1 is not None:
-        module.__flops__ += output.numel()
+        module.total_ops += output.numel()
     if module.gamma_2 is not None:
-        module.__flops__ += output.numel()
-    module.__flops__ += output.numel() * 2
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+        module.total_ops += output.numel()
+    module.total_ops += output.numel() * 2
+    module.total_ops += sum_submodule_ops(module)
 
 
 def swin_window_attention(module, input, output):
@@ -74,22 +79,20 @@ def swin_window_attention(module, input, output):
     B, N, C = input[0].shape
     attn_dim = module.qkv.out_features / 3
     # q,k,v shape: B, num_heads, N, attn_dim
-    module.__flops__ += B * num_heads * N * attn_dim  # scale
-    module.__flops__ += B * num_heads * N * (attn_dim * N
+    module.total_ops += B * num_heads * N * attn_dim  # scale
+    module.total_ops += B * num_heads * N * (attn_dim * N
                                              )  # q @ k.transpose(-2, -1)
-    module.__flops__ += B * num_heads * N * N  # attn + self._get_rel_pos_bias()
+    module.total_ops += B * num_heads * N * N  # attn + self._get_rel_pos_bias()
     if len(input) > 1 and input[1] is not None:
-        module.__flops__ += B * num_heads * N * N
-    module.__flops__ += B * num_heads * N * (N * attn_dim)  # (attn @ v)
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+        module.total_ops += B * num_heads * N * N
+    module.total_ops += B * num_heads * N * (N * attn_dim)  # (attn @ v)
+    module.total_ops += sum_submodule_ops(module)
 
 
 def softmax(module, input, output):
     x = input[0]
     dim = x.shape[module.dim]
-    module.__flops__ += x.numel() / dim * (2 * dim)
+    module.total_ops += x.numel() / dim * (2 * dim)
 
 
 def msdeform_attn_softmax(module, input, output):
@@ -99,24 +102,20 @@ def msdeform_attn_softmax(module, input, output):
     n_points = module.n_points
     dim = C / n_heads
     # attention
-    module.__flops__ += N * Len_q * n_heads * n_levels * (n_points * dim)
+    module.total_ops += N * Len_q * n_heads * n_levels * (n_points * dim)
     # attention_weights shape: N, Len_q, n_heads, n_levels * n_points
     # attention_weights softmax
-    module.__flops__ += N * Len_q * n_heads * (2 * n_levels * n_points)
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+    module.total_ops += N * Len_q * n_heads * (2 * n_levels * n_points)
+    module.total_ops += sum_submodule_ops(module)
 
 
 def dcnv3(module, input, output):
     x = input[0][0]
     if not module.layer_scale:
-        module.__flops__ += 2 * x.numel()
+        module.total_ops += 2 * x.numel()
     else:
-        module.__flops__ += 4 * x.numel()
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+        module.total_ops += 4 * x.numel()
+    module.total_ops += sum_submodule_ops(module)
 
 
 def pvt_v2_atten(module, input, output):
@@ -125,36 +124,30 @@ def pvt_v2_atten(module, input, output):
     dim = C // num_heads
     # q shape: B, num_heads, N,  C // num_heads
     # k v shape:  B, num_heads, N, C // num_heads
-    module.__flops__ += B * num_heads * N * (dim * N
+    module.total_ops += B * num_heads * N * (dim * N
                                              )  # q @ k.transpose(-2, -1)
-    module.__flops__ += B * num_heads * N * N  # scale
-    module.__flops__ += B * num_heads * N * (2 * N)  # softmax
-    module.__flops__ += B * num_heads * N * (N * dim)  # attn @ v
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+    module.total_ops += B * num_heads * N * N  # scale
+    module.total_ops += B * num_heads * N * (2 * N)  # softmax
+    module.total_ops += B * num_heads * N * (N * dim)  # attn @ v
+    module.total_ops += sum_submodule_ops(module)
 
 
 def pvt_v2_block(module, input, output):
     x = input[0]
     if module.gamma_1 is not None:
-        module.__flops__ += x.numel()
+        module.total_ops += x.numel()
     if module.gamma_2 is not None:
-        module.__flops__ += x.numel()
-    module.__flops__ += 2 * x.numel()
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+        module.total_ops += x.numel()
+    module.total_ops += 2 * x.numel()
+    module.total_ops += sum_submodule_ops(module)
 
 
 def convnext_block(module, input, output):
     x = input[0]
     if module.gamma is not None:
-        module.__flops__ += x.numel()
-    module.__flops__ += x.numel()
-    for m in module.modules():
-        if hasattr(m, '__flops__'):
-            module.__flops__ += m.__flops__
+        module.total_ops += x.numel()
+    module.total_ops += x.numel()
+    module.total_ops += sum_submodule_ops(module)
 
 
 custom_modules_hooks = {
@@ -177,16 +170,13 @@ def main(args):
     model = create_model(args.model_name, pretrained=False,
                          num_classes=1000).eval().cuda()
 
-    flops, params = get_model_complexity_info(
-        model, (3, 224, 224),
-        as_strings=False,
-        print_per_layer_stat=False,
-        custom_modules_hooks=custom_modules_hooks,
-        verbose=True)
+    macs, params = profile(model, (torch.randn(1, 3, 224, 224).cuda(), ),
+                           custom_ops=custom_modules_hooks,
+                           verbose=False)
 
-    flops = str(round(flops / 10.**9, 2))
+    macs = str(round(macs / 10.**9, 2))
     params = str(round(params / 10**6, 2))
-    print("model_name: MACs {}G, Params {}M".format(flops, params))
+    print("model_name: MACs {}G, Params {}M".format(macs, params))
 
 
 if __name__ == '__main__':