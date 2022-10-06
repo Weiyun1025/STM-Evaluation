6c630ff6a00a2d126352cb1f01334c462ee83360
1612893408@qq.com <1612893408@qq.com>
2022-10-06 21:58:41 +0800

    update speed_eval shell

---
+++ shell/eval/speed_eval.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -x
+mkdir logs
+
+PARTITION=VC
+GPUS_PER_NODE=${GPUS_PER_NODE:-1}
+
+export CUDA_LAUNCH_BLOCKING=1
+
+kernprof -l test_speed.py
+
+srun -p ${PARTITION} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    python -m line_profiler test_speed.py.lprof
+    
