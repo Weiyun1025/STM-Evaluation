91717123cd55c835b476d96ecea397d1a5cb0d4b
1612893408@qq.com <1612893408@qq.com>
2022-10-06 21:58:04 +0800

    save more ckpt

--- utils.py
+++ utils.py
@@ -487,7 +487,7 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler, mo
     if is_main_process() and isinstance(epoch, int):
         to_del = epoch - args.save_ckpt_num * args.save_ckpt_freq
         old_ckpt = output_dir / ('checkpoint-%s.pth' % to_del)
-        if os.path.exists(old_ckpt):
+        if os.path.exists(old_ckpt) and (to_del + 1) % 50 != 0:
             os.remove(old_ckpt)
 
 
