78aed6179cecef29fd251ccda1b5b9c3ce1be157
1612893408@qq.com <1612893408@qq.com>
2022-10-07 00:10:07 +0800

    update speed_eval.

--- shell/eval/speed_eval.sh
+++ shell/eval/speed_eval.sh
@@ -8,7 +8,7 @@ GPUS_PER_NODE=${GPUS_PER_NODE:-1}
 
 export CUDA_LAUNCH_BLOCKING=1
 
-kernprof -l speed_eval.py
+kernprof -l speed_eval.py --model_type $1
 
 srun -p ${PARTITION} \
     --gres=gpu:${GPUS_PER_NODE} \
