aec63e720aab3deb7e4e05ac176494cf7004bb92
1612893408@qq.com <1612893408@qq.com>
2022-10-07 20:16:12 +0800

    update

--- models/blocks/halonet_timm.py
+++ models/blocks/halonet_timm.py
@@ -195,7 +195,7 @@ class HaloBlock(nn.Module):
 
 
 class HaloBlockV2(nn.Module):
-    def __init__(self, dim, drop_path, layer_scale_init_value,
+    def __init__(self, dim, drop_path, layer_scale_init_value, padding_value,
                  block_size, halo_size, stage, depth, num_heads,
                  mlp_ratio=4., drop=0., act_layer=nn.GELU, norm_layer=LayerNorm2d,
                  **kwargs):
@@ -213,7 +213,8 @@ class HaloBlockV2(nn.Module):
         self.norm1 = norm_layer(dim // stride)
         self.attn = HaloAttention(dim=dim // stride, dim_out=dim,
                                   num_heads=num_heads[stage], stride=stride,
-                                  block_size=block_size, halo_size=halo_size)
+                                  block_size=block_size, halo_size=halo_size,
+                                  padding_value=padding_value)
 
         self.gamma_1 = nn.Parameter(layer_scale_init_value * torch.ones((1, dim, 1, 1)),
                                     requires_grad=True) if layer_scale_init_value > 0 else None
