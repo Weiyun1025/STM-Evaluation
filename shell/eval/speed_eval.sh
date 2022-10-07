963a7e1c9db916f4ddcad2098fae830d126612ff
1612893408@qq.com <1612893408@qq.com>
2022-10-07 21:24:14 +0800

    update shell

--- shell/eval/speed_eval.sh
+++ shell/eval/speed_eval.sh
@@ -8,15 +8,8 @@ PARTITION=VC
 GPUS_PER_NODE=${GPUS_PER_NODE:-1}
 MODEL=$1
 
-#export CUDA_LAUNCH_BLOCKING=1
-
 srun -p ${PARTITION} \
     --gres=gpu:${GPUS_PER_NODE} \
     --quotatype=spot \
-    kernprof -l speed_eval.py --model_type ${MODEL}
-
-srun -p ${PARTITION} \
-    --gres=gpu:${GPUS_PER_NODE} \
-    --quotatype=spot \
-    python -m line_profiler speed_eval.py.lprof 1>"logs/speed_eval/${MODEL}.out"
+    python speed_eval.py --model_type ${MODEL} 1>"logs/speed_eval/${MODEL}.out"
     
