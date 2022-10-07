a908c27af04926d747d2fb25aaadf89d547bd093
1612893408@qq.com <1612893408@qq.com>
2022-10-07 13:11:29 +0800

    update shell

--- shell/eval/speed_eval.sh
+++ shell/eval/speed_eval.sh
@@ -2,16 +2,18 @@
 
 set -x
 mkdir logs
+mkdir logs/profile
 
 PARTITION=VC
 GPUS_PER_NODE=${GPUS_PER_NODE:-1}
+MODEL_TYPE=$1
 
 export CUDA_LAUNCH_BLOCKING=1
 
-kernprof -l speed_eval.py --model_type $1
+kernprof -l speed_eval.py --model_type ${MODEL_TYPE}
 
 srun -p ${PARTITION} \
     --gres=gpu:${GPUS_PER_NODE} \
     --quotatype=spot \
-    python -m line_profiler speed_eval.py.lprof
+    python -m line_profiler speed_eval.py.lprof 1>"logs/profile/${MODEL_TYPE}.out"
     
