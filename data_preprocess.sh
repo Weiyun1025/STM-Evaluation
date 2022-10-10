9edebc3a1ff4ce0da2437cd9504194f625cf7282
1612893408@qq.com <1612893408@qq.com>
2022-10-10 19:21:32 +0800

    update data

--- data_preprocess.sh
+++ data_preprocess.sh
@@ -2,5 +2,5 @@
 
 set -x
 mkdir /root/ImageNet/meta
-cp ./minidata/train.txt /root/ImageNet/meta
-cp ./minidata/val.txt /root/ImageNet/meta
+cp ./minidata/train.txt /root/ImageNet/meta/train.txt
+cp ./minidata/modified_val.txt /root/ImageNet/meta/val.txt
