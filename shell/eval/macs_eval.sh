fdd6a8e7edd0c1a2e109a0ec9c53c60b4dfde3aa
1612893408@qq.com <1612893408@qq.com>
2022-10-11 10:04:50 +0800

    update shell

--- shell/eval/macs_eval.sh
+++ shell/eval/macs_eval.sh
@@ -9,9 +9,9 @@ GPUS_PER_NODE=${GPUS_PER_NODE:-1}
 model_list=('conv_swin' 'conv_halo_v2_timm' 'conv_pvt' 'conv_pvt_v2' 'conv_convnext_v2')
 scale_list=('micro' 'tiny' 'small' 'base')
 
-for model_name in ${model_list[@]}
+for model_name in "${model_list[@]}"
 do
-    for scale in ${scale_list[@]}
+    for scale in "${scale_list[@]}"
     do
         srun -p ${PARTITION} \
             --gres=gpu:${GPUS_PER_NODE} \
