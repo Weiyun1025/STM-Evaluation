ead13662dda97740bb8f6db5b80dc51cb1e86ed1
1612893408@qq.com <1612893408@qq.com>
2022-10-27 23:37:09 +0800

    update v6

--- models/blocks/convnext.py
+++ models/blocks/convnext.py
@@ -278,28 +278,29 @@ class ConvNeXtV5Block(nn.Module):
 
 class ConvNeXtV6Block(nn.Module):
     # double res + in/out proj
-    def __init__(self, dim, drop_path, layer_scale_init_value, **kwargs):
+    def __init__(self, dim, drop_path, layer_scale_init_value, bias=False, **kwargs):
         super().__init__()
         self.pre_pwnorm = LayerNorm2d(dim, eps=1e-6)
-        self.pre_pwconv = nn.Conv2d(dim, dim, kernel_size=1, stride=1, padding=0)
-
+        self.pre_pwconv = nn.Conv2d(dim, dim, kernel_size=1, stride=1, padding=0, bias=bias)
         self.gamma_1 = nn.Parameter(layer_scale_init_value * torch.ones((1, dim, 1, 1)),
                                     requires_grad=True) if layer_scale_init_value > 0 else None
 
         self.dwnorm = LayerNorm2d(dim, eps=1e-6)
         self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)
+        self.gamma_2 = nn.Parameter(layer_scale_init_value * torch.ones((1, dim, 1, 1)),
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
 
         # pointwise/1x1 convs, implemented with linear layers
         self.post_pwnorm = nn.LayerNorm(dim, eps=1e-6)
         self.post_pwconv = nn.Sequential(
-            nn.Linear(dim, dim),
+            nn.Linear(dim, dim, bias=bias),
             nn.GELU(),
             nn.Linear(dim, 4 * dim),
             nn.GELU(),
             nn.Linear(4 * dim, dim),
         )
 
-        self.gamma_2 = nn.Parameter(layer_scale_init_value * torch.ones((1, 1, 1, dim)),
+        self.gamma_3 = nn.Parameter(layer_scale_init_value * torch.ones((1, 1, 1, dim)),
                                     requires_grad=True) if layer_scale_init_value > 0 else None
 
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
@@ -311,15 +312,19 @@ class ConvNeXtV6Block(nn.Module):
             x = self.gamma_1 * x
         x = shortcut + self.drop_path(x)
 
+        shortcut = x
         x = self.dwconv(self.dwnorm(x))
+        if self.gamma_2 is not None:
+            x = self.gamma_2 * x
+        x = shortcut + self.drop_path(x)
 
         # (N, C, H, W) -> (N, H, W, C)
         x = x.permute(0, 2, 3, 1).contiguous()
 
         shortcut = x
         x = self.post_pwconv(self.post_pwnorm(x))
-        if self.gamma_2 is not None:
-            x = self.gamma_2 * x
+        if self.gamma_3 is not None:
+            x = self.gamma_3 * x
         x = shortcut + self.drop_path(x)
 
         # (N, H, W, C) -> (N, C, H, W)
