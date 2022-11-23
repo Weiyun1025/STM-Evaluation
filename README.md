f9951a9ecf4246a66ecdaeb3c7f0c5831c3e2a08
1612893408@qq.com <1612893408@qq.com>
2022-11-23 16:09:42 +0800

    clean ablation model family

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Requirements
 
-The code requires to install PyTorch 1.12, torch vision and timm.
+The code requires to install PyTorch 1.12, torch vision, timm and MultiScaleDeformableAttention.
 
-To install the 
+To install the MultiScaleDeformableAttention
+
+```shell
+cd ./ops
+sh make.sh
+```
 
 
 # Usage
@@ -12,7 +17,31 @@ To install the
 - To train a unified model on ImageNet-1K
 
 ```shell
-sh shells/1k_pretrain
+# training setting for unified_halonet, unified_swin and unified_pvt
+sh shells/1k_pretrain/transformer.sh [model_name]
+
+# training setting for unified_convnext_v3 and unified_dcn_v3
+sh shells/1k_pretrain/cnn.sh [model_name]
+```
+
+or
+
+```shell
+torchrun \
+    --nnodes=${NUM_NODES} \
+    --nproc_per_node=${GPUS_PER_NODE} \
+		main.py \
+    --model ${MODEL} \
+    --epochs 300 \
+    --update_freq ${UPDATE_FREQ} \
+    --batch_size ${BATCH_SIZE} \
+    --lr ${LR} \
+    --warmup_init_lr ${INIT_LR} \
+    --min_lr ${END_LR} \
+    --data_set IMNET1k \
+    --data_path /mnt/cache/share/images/ \
+    --nb_classes 1000 \
+    --output_dir "output_dir"
 ```
 
 ## Evaluation
@@ -20,7 +49,26 @@ sh shells/1k_pretrain
 - To evaluate a unified model on ImageNet-1K
 
 ```shell
-sh shells/eval/eval.sh [model_name]
+sh shells/eval/eval.sh [model_name] [ckpt_path]
+```
+
+or
+
+```shell
+torchrun \
+    --nnodes=${NUM_NODES} \
+    --nproc_per_node=${GPUS_PER_NODE} \
+    main.py \
+    --model ${MODEL} \
+    --eval true \
+    --resume ${CKPT} \
+    --batch_size ${BATCH_SIZE} \
+    --input_size 224 \
+    --data_set IMNET1k \
+    --data_path /mnt/cache/share/images/ \
+    --data_on_memory false \
+    --nb_classes 1000 \
+    --use_amp false \
 ```
 
 
--- models/model_factory/halonet_factory.py
+++ models/model_factory/halonet_factory.py
@@ -4,7 +4,7 @@ from ..blocks.halonet import HaloBlockV2, HaloSingleResBlock
 
 
 @register_model
-def unified_halo_v2_micro(pretrained=False, **kwargs):
+def unified_halonet_micro(pretrained=False, **kwargs):
     dims = [32 * 2 ** i for i in range(4)]
     depths = [2, 2, 9, 2]
     num_heads = [1, 2, 4, 8]
@@ -29,7 +29,7 @@ def unified_halo_v2_micro(pretrained=False, **kwargs):
 
 
 @register_model
-def unified_halo_v2_tiny(pretrained=False, **kwargs):
+def unified_halonet_tiny(pretrained=False, **kwargs):
     dims = [96 * 2 ** i for i in range(4)]
     depths = [2, 2, 6, 2]
     num_heads = [3, 6, 12, 24]
@@ -54,7 +54,7 @@ def unified_halo_v2_tiny(pretrained=False, **kwargs):
 
 
 @register_model
-def unified_halo_v2_small(pretrained=False, **kwargs):
+def unified_halonet_small(pretrained=False, **kwargs):
     dims = [96 * 2 ** i for i in range(4)]
     depths = [2, 2, 18, 2]
     num_heads = [3, 6, 12, 24]
@@ -79,7 +79,7 @@ def unified_halo_v2_small(pretrained=False, **kwargs):
 
 
 @register_model
-def unified_halo_v2_base(pretrained=False, **kwargs):
+def unified_halonet_base(pretrained=False, **kwargs):
     dims = [128 * 2 ** i for i in range(4)]
     depths = [2, 2, 18, 2]
     num_heads = [4, 8, 16, 32]
@@ -95,111 +95,6 @@ def unified_halo_v2_base(pretrained=False, **kwargs):
                                        halo_size=halo_size,
                                        pos_embed_type='query_related'),
                      drop_path_rate=0.5,
-                     **kwargs)
-
-    if pretrained:
-        raise NotImplementedError()
-
-    return model
-
-
-@register_model
-def unified_halo_v2_b_small(pretrained=False, **kwargs):
-    dims = [96 * 2 ** i for i in range(4)]
-    depths = [2, 2, 18, 2]
-    num_heads = [3, 6, 12, 24]
-    block_size = 7
-    halo_size = 3
-
-    model = MetaArch(img_size=224,
-                     depths=depths,
-                     dims=dims,
-                     block_type=HaloBlockV2,
-                     block_kwargs=dict(num_heads=num_heads,
-                                       block_size=block_size,
-                                       halo_size=halo_size,
-                                       pos_embed_type='query_related'),
-                     norm_after_avg=True,
-                     drop_path_rate=0.3,
-                     **kwargs)
-
-    if pretrained:
-        raise NotImplementedError()
-
-    return model
-
-
-@register_model
-def unified_halo_v2_c_small(pretrained=False, **kwargs):
-    dims = [96 * 2 ** i for i in range(4)]
-    depths = [2, 2, 18, 2]
-    num_heads = [3, 6, 12, 24]
-    block_size = 7
-    halo_size = 3
-
-    model = MetaArch(img_size=224,
-                     depths=depths,
-                     dims=dims,
-                     block_type=HaloBlockV2,
-                     block_kwargs=dict(num_heads=num_heads,
-                                       block_size=block_size,
-                                       halo_size=halo_size,
-                                       pos_embed_type='query_related'),
-                     norm_every_stage=False,
-                     norm_after_avg=True,
-                     drop_path_rate=0.3,
-                     **kwargs)
-
-    if pretrained:
-        raise NotImplementedError()
-
-    return model
-
-
-@register_model
-def unified_halo_v2_d_small(pretrained=False, **kwargs):
-    dims = [96 * 2 ** i for i in range(4)]
-    depths = [2, 2, 18, 2]
-    num_heads = [3, 6, 12, 24]
-    block_size = 7
-    halo_size = 3
-
-    model = MetaArch(img_size=224,
-                     depths=depths,
-                     dims=dims,
-                     block_type=HaloSingleResBlock,
-                     block_kwargs=dict(num_heads=num_heads,
-                                       block_size=block_size,
-                                       halo_size=halo_size,
-                                       pos_embed_type='query_related'),
-                     drop_path_rate=0.3,
-                     **kwargs)
-
-    if pretrained:
-        raise NotImplementedError()
-
-    return model
-
-
-@register_model
-def unified_halo_v2_e_small(pretrained=False, **kwargs):
-    dims = [96 * 2 ** i for i in range(4)]
-    depths = [2, 2, 18, 2]
-    num_heads = [3, 6, 12, 24]
-    block_size = 7
-    halo_size = 3
-
-    model = MetaArch(img_size=224,
-                     depths=depths,
-                     dims=dims,
-                     block_type=HaloSingleResBlock,
-                     block_kwargs=dict(num_heads=num_heads,
-                                       block_size=block_size,
-                                       halo_size=halo_size,
-                                       pos_embed_type='query_related'),
-                     norm_every_stage=False,
-                     norm_after_avg=True,
-                     drop_path_rate=0.3,
                      **kwargs)
 
     if pretrained:
