497880d956e0f778cb7b73420ce1001328ff39e1
1612893408@qq.com <1612893408@qq.com>
2022-11-24 17:15:30 +0800

    update ablation models

--- models/model_factory/halonet_factory.py
+++ models/model_factory/halonet_factory.py
@@ -94,6 +94,58 @@ def unified_halonet_small(pretrained=False, **kwargs):
 
 
 @register_model
+def unified_halonet_h1_small(pretrained=False, **kwargs):
+    dims = [96 * 2 ** i for i in range(4)]
+    depths = [2, 2, 18, 2]
+    num_heads = [3, 6, 12, 24]
+
+    img_size = kwargs.get('img_size', 224)
+    block_size = block_size_dict[img_size]
+    halo_size = 1
+
+    model = MetaArch(depths=depths,
+                     dims=dims,
+                     block_type=HaloBlockV2,
+                     block_kwargs=dict(num_heads=num_heads,
+                                       block_size=block_size,
+                                       halo_size=halo_size,
+                                       pos_embed_type='query_related'),
+                     drop_path_rate=0.3,
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+
+@register_model
+def unified_halonet_h2_small(pretrained=False, **kwargs):
+    dims = [96 * 2 ** i for i in range(4)]
+    depths = [2, 2, 18, 2]
+    num_heads = [3, 6, 12, 24]
+
+    img_size = kwargs.get('img_size', 224)
+    block_size = block_size_dict[img_size]
+    halo_size = 2
+
+    model = MetaArch(depths=depths,
+                     dims=dims,
+                     block_type=HaloBlockV2,
+                     block_kwargs=dict(num_heads=num_heads,
+                                       block_size=block_size,
+                                       halo_size=halo_size,
+                                       pos_embed_type='query_related'),
+                     drop_path_rate=0.3,
+                     **kwargs)
+
+    if pretrained:
+        raise NotImplementedError()
+
+    return model
+
+
+@register_model
 def unified_halonet_base(pretrained=False, **kwargs):
     dims = [128 * 2 ** i for i in range(4)]
     depths = [2, 2, 18, 2]
