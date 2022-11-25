2a618b480d3412cf95efdd0a60d723f495abfbf8
1612893408@qq.com <1612893408@qq.com>
2022-11-25 17:33:23 +0800

    update swin

--- models/blocks/swin.py
+++ models/blocks/swin.py
@@ -1,9 +1,9 @@
 import torch
+import torch.nn.functional as F
 from torch import nn
-from timm.models import register_model
 from timm.models.layers import DropPath, Mlp, to_2tuple, _assert
 from timm.models.swin_transformer import WindowAttention, window_partition, window_reverse
-from ..meta_arch import MetaArch, LayerNorm2d
+from ..meta_arch import LayerNorm2d
 
 
 class SwinBlock(nn.Module):
@@ -112,6 +112,24 @@ class SwinBlock(nn.Module):
         x = shortcut + self.drop_path(x)
 
         return x
+
+    def load_state_dict(self, state_dict, strict: bool = True):
+        new_state_dict = {}
+        for key, value in state_dict.items():
+            if 'relative_position_bias_table' in key:
+                L1, nH1 = value.shape
+                S1 = int(L1 ** 0.5)
+                S2 = self.window_size * 2 - 1
+
+                value = value.permute(1, 0).view(1, nH1, S1, S1)
+                value = F.interpolate(value,
+                                      size=(S2, S2),
+                                      mode='bicubic')
+                value = value.view(nH1, S2 * S2).permute(1, 0)
+
+            new_state_dict[key] = value
+
+        return super().load_state_dict(new_state_dict, strict)
 
 
 class SwinBlockNoSwitch(nn.Module):
