e5cf06a094e86ad59279a8129ffdd5030a84d019
1612893408@qq.com <1612893408@qq.com>
2022-11-26 14:26:07 +0800

    update shells

--- main.py
+++ main.py
@@ -233,7 +233,9 @@ def main(args):
     np.random.seed(seed)
     cudnn.benchmark = True
 
-    dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
+    if args.eval:
+        dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
+
     if args.disable_eval:
         args.dist_eval = False
         dataset_val = None
